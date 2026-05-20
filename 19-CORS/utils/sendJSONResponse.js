aae9ac35d359b780d844d79848f869870e307d2a
muskanchawla11 <muskanchawla115@gmail.com>
2026-05-20 23:21:51 +0530

    Update 19-CORS with CORS implementation

---
+++ 19-CORS/utils/sendJSONResponse.js
@@ -0,0 +1,9 @@
+export const sendJSONResponse = (res, statusCode, payload) => {
+    res.setHeader('Content-Type', 'application/json')
+    //res.setHeader('Access-Control-Allow-Origin', '*')
+    //res.setHeader('Access-Control-Allow-Methods', 'GET')
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Methods','GET')
+    res.statusCode = statusCode
+    res.end(JSON.stringify(payload))
+}
