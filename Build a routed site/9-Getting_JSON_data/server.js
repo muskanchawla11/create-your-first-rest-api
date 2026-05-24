2a53c74d24599ea2335c358f4ccff42564752658
muskanchawla11 <muskanchawla115@gmail.com>
2026-05-24 23:48:02 +0530

    Add 9-Getting_JSON_data folder

---
+++ Build a routed site/9-Getting_JSON_data/server.js
@@ -0,0 +1,18 @@
+import http from 'node:http'
+import { serveStatic } from './utils/serveStatic.js'
+import { getData } from './utils/getData.js'
+
+const PORT = 8000
+
+const __dirname = import.meta.dirname
+
+console.log(getData())
+
+const server = http.createServer(async (req, res) => {
+
+
+    await serveStatic(req, res, __dirname)
+}) 
+
+server.listen(PORT, ()=> console.log(`Connected on port: ${PORT}`))
+ 
